f6099b7303c4f2fac34b71d9962dd1b4c66e01e4
SantiagoCumbal <151807183+SantiagoCumbal@users.noreply.github.com>
2025-08-27 23:42:16 -0500

    new 1

--- src/routers/Reserva_router.js
+++ src/routers/Reserva_router.js
@@ -3,11 +3,11 @@ import { verificarTokenJWT } from '../middlewares/JWT.js'
 import { actualizarReserva, detalleReserva, eliminarReserva, registroReserva, visualizarReservas } from '../controllers/Reserva_controller.js'
 
 const router = Router()
-router.post('/registro/matricula',verificarTokenJWT, registroReserva)
-router.get('/matriculas',verificarTokenJWT, visualizarReservas)
-router.get('/matricula/:id', verificarTokenJWT,detalleReserva)
-router.put('/matricula/actualizar/:id',verificarTokenJWT, actualizarReserva)
-router.delete('/matricula/eliminar/:id',verificarTokenJWT, eliminarReserva)
+router.post('/registro/reserva',verificarTokenJWT, registroReserva)
+router.get('/reservas',verificarTokenJWT, visualizarReservas)
+router.get('/reserva/:id', verificarTokenJWT,detalleReserva)
+router.put('/reserva/actualizar/:id',verificarTokenJWT, actualizarReserva)
+router.delete('/reserva/eliminar/:id',verificarTokenJWT, eliminarReserva)
 
 
 export default router
